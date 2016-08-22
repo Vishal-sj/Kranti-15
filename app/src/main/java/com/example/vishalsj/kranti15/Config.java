a9a7a249735bb07496f3f486a61a755b351c4dc7
Vishal_SJ <lashiv.21@gmail.com>
2016-08-23 00:41:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/vishalsj/kranti15/Config.java
@@ -0,0 +1,13 @@
+package com.example.vishalsj.kranti15;
+
+/**
+ * Created by lashi on 14-Aug-16.
+ */
+public final class Config {
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAZBun7lO7fJoL97kjeeEM_A_cDQaoM0TI";
+
+    private Config() {
+    }
+
+}
